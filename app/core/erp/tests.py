7a0bbbefcb8ff52a49d0a4715346ad5752cde603
EDIN ELICEO ISAAC MORAN CHEN <eliceomoran@gmail.com>
2022-10-12 23:10:39 -0600

    update video 13

--- app/core/erp/tests.py
+++ app/core/erp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from config.wsgi import *
+# Create your tests here.
+from core.erp.models import Category
+
+
+
+query = Category.objects.all()
+print(query)
