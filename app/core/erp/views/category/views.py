ddebd10549d2690559af4095def21667c2de2319
EDIN ELICEO ISAAC MORAN CHEN <eliceomoran@gmail.com>
2022-10-22 09:27:53 -0600

    primer formulario

--- app/core/erp/views/category/views.py
+++ app/core/erp/views/category/views.py
@@ -2,8 +2,12 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
+
+from core.erp.forms import CategoryForm
 from core.erp.models import Category
 from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 def category_list(request):
 #json
@@ -22,6 +26,7 @@ class CategoryListView(ListView):
     #@method_decorator(login_required):
 
     @method_decorator(csrf_exempt)
+
     def dispatch(self, request, *args, **kwargs):
        # if request.method == 'GET':
             #llamar el metodo redirect, redireccionar
@@ -29,8 +34,14 @@ class CategoryListView(ListView):
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        data = {'name': 'Edin'}
-        return JsonResponse(data)
+       # data = {'name': 'Edin'}
+       data = {}
+       print(request.POST)
+       try:
+           data = Category.objects.get(pk=request.POST['id']).toJSON()
+       except Exception as e:
+           data['error'] = str(e)
+       return JsonResponse(data)
 
 #opost y get
 
@@ -39,3 +50,15 @@ class CategoryListView(ListView):
         context['title'] = 'Listado de Categorías'
         return context
 
+
+
+class CategoryCreateView(CreateView):
+   model = Category
+   form_class = CategoryForm
+   template_name = 'category/create.html'
+   success_url = reverse_lazy('erp:category_list')
+
+   def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['title'] = 'Creación una Categoria'
+       return context
