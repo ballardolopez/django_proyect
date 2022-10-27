56ced27dd62bb2e16887bb0137a22150ee58dadd
EDIN ELICEO ISAAC MORAN CHEN <eliceomoran@gmail.com>
2022-10-26 20:45:30 -0600

    correccion video 35

--- app/config/settings.py
+++ app/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig',
+    #'core.apps.CoreConfig',
     # libs
     'widget_tweaks',
     # Apps
